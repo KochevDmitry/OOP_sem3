4fb8c7d8093e2d2a94bf91c497024e0e78349e8b
KochevDmitry <65921394+KochevDmitry@users.noreply.github.com>
2023-11-06 16:44:24 +0300

    Update gtests.cpp

--- lab2/gtests.cpp
+++ lab2/gtests.cpp
@@ -207,6 +207,352 @@ TEST(test_10, basic_test_set) {
     ASSERT_TRUE(answer);
 }
 
+TEST(test_11, basic_test_set) {
+    unsigned char* str1 = new unsigned char[6];
+    str1[0] = '1';
+    str1[1] = '1';
+    str1[2] = '1';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    BitString a(6, str1);
+    BitString c;
+    BitString d = a&c;
+    bool answer = false;
+    if (d.GetBit(0) == '0' && d.GetBit(1) == '0' && d.GetBit(2) == '0' && d.GetBit(3) == '0' &&
+         d.GetBit(4) == '0' && d.GetBit(5) == '0' && d.GetLength() == 6)
+         answer = true;
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+TEST(test_12, basic_test_set) {
+   unsigned char* str1 = new unsigned char[6];
+    str1[0] = '1';
+    str1[1] = '1';
+    str1[2] = '1';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    BitString a(6, str1);
+    BitString c;
+    BitString d = a^c;
+    bool answer = false;
+    if (d.GetBit(0) == '1' && d.GetBit(1) == '1' && d.GetBit(2) == '1' && d.GetBit(3) == '0' &&
+         d.GetBit(4) == '1' && d.GetBit(5) == '0' && d.GetLength() == 6)
+         answer = true;
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+TEST(test_13, basic_test_set) {
+   unsigned char* str1 = new unsigned char[6];
+    str1[0] = '1';
+    str1[1] = '1';
+    str1[2] = '1';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    BitString a(6, str1);
+    BitString c;
+    BitString d = a|c;
+    bool answer = false;
+    if (d.GetBit(0) == '1' && d.GetBit(1) == '1' && d.GetBit(2) == '1' && d.GetBit(3) == '0' &&
+         d.GetBit(4) == '1' && d.GetBit(5) == '0' && d.GetLength() == 6)
+         answer = true;
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+TEST(test_14, basic_test_set) {
+    BitString c;
+    BitString d = ~c;
+    bool answer = false;
+    if (d.GetLength() == 0)
+         answer = true;
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_15, basic_test_set) {
+    BitString a;
+    BitString c;
+    BitString d = a&c;
+    bool answer = false;
+    if (d.GetLength() == 0)
+         answer = true;
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_16, basic_test_set) {
+    BitString a;
+    BitString c;
+    BitString d = a^c;
+    bool answer = false;
+    if (d.GetLength() == 0)
+         answer = true;
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_17, basic_test_set) {
+    BitString a;
+    BitString c;
+    BitString d = a|c;
+    bool answer = false;
+    if (d.GetLength() == 0)
+         answer = true;
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_18, basic_test_set) {
+    unsigned char* str1 = new unsigned char[6];
+    str1[0] = '1';
+    str1[1] = '1';
+    str1[2] = '1';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    BitString a(6, str1);
+    BitString c(a);
+    bool answer = false;
+    if (c.GetBit(0) == '1' && c.GetBit(1) == '1' && c.GetBit(2) == '1' && c.GetBit(3) == '0' &&
+         c.GetBit(4) == '1' && c.GetBit(5) == '0' && c.GetLength() == 6)
+         answer = true;
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+
+
+
+// TEST(test_19, basic_test_set) {
+//     int* str1 = new int[6];
+//     str1[0] = 1;
+//     str1[1] = 1;
+//     str1[2] = 1;
+//     str1[3] = 0;
+//     str1[4] = 1;
+//     str1[5] = 0;
+//     bool answer = false;
+//     try{
+//         BitString first(6, str1);
+//     }
+//     catch(std::exception &ex){
+//         bool answer = true;
+//     }
+//     ASSERT_TRUE(answer);
+// }
+
+TEST(test_19, basic_test_set) {
+    unsigned char* str1 = new unsigned char[6];
+    str1[0] = 'A';
+    str1[1] = 'B';
+    str1[2] = 'A';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    bool answer = false;
+    try{
+        BitString first(6, str1);
+    }
+    catch(std::exception &ex){
+        answer = true;
+    }
+    delete[] str1;
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_20, basic_test_set) {
+    unsigned char* str1 = new unsigned char[6];
+    str1[0] = '1';
+    str1[1] = '0';
+    str1[2] = '0';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    bool answer = false;
+    try{
+        BitString first(-1, str1);
+    }
+    catch(std::exception &ex){
+        answer = true;
+    }
+    delete[] str1;
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_21, basic_test_set) {
+    bool answer = false;
+    try{
+        BitString first(-10);
+    }
+    catch(std::exception &ex){
+        answer = true;
+    }
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_22, basic_test_set) {
+    unsigned char* str1 = new unsigned char[6];
+    str1[0] = '1';
+    str1[1] = '0';
+    str1[2] = '0';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    bool answer = false;
+    try{
+        BitString first(8, str1);
+    }
+    catch(std::exception &ex){
+        answer = true;
+    }
+    delete[] str1;
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_23, basic_test_set) {
+    unsigned char* str1 = new unsigned char[6];
+    str1[0] = '1';
+    str1[1] = '0';
+    str1[2] = '0';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    BitString first(6, str1);
+    bool answer = false;
+    try{
+        first.SetBit(10, '1');
+    }
+    catch(std::exception &ex){
+        answer = true;
+    }
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_24, basic_test_set) {
+    unsigned char* str1 = new unsigned char[6];
+    str1[0] = '1';
+    str1[1] = '0';
+    str1[2] = '0';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    BitString first(6, str1);
+    bool answer = false;
+    try{
+        first.SetBit(1, 'A');
+    }
+    catch(std::exception &ex){
+        answer = true;
+    }
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_25, basic_test_set) {
+    BitString first;
+    unsigned char* str1 = new unsigned char[6];
+    str1[0] = 'A';
+    str1[1] = 'B';
+    str1[2] = 'A';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    bool answer = false;
+    try{
+        first.SetStringAndLength(6, str1);
+    }
+    catch(std::exception &ex){
+        answer = true;
+    }
+    delete[] str1;
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_26, basic_test_set) {
+    BitString first;
+    unsigned char* str1 = new unsigned char[6];
+    str1[0] = '1';
+    str1[1] = '0';
+    str1[2] = '0';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    bool answer = false;
+    try{
+        first.SetStringAndLength(-1, str1);
+    }
+    catch(std::exception &ex){
+        answer = true;
+    }
+    delete[] str1;
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_27, basic_test_set) {
+    BitString first;
+    unsigned char* str1 = new unsigned char[6];
+    str1[0] = '1';
+    str1[1] = '0';
+    str1[2] = '0';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    bool answer = false;
+    try{
+        first.SetStringAndLength(10, str1);
+    }
+    catch(std::exception &ex){
+        answer = true;
+    }
+    delete[] str1;
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_28, basic_test_set) {
+    unsigned char* str1 = new unsigned char[6];
+    str1[0] = '1';
+    str1[1] = '0';
+    str1[2] = '0';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    bool answer = false;
+    try{
+        BitString first(4, str1);
+    }
+    catch(std::exception &ex){
+        answer = true;
+    }
+    delete[] str1;
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+
+TEST(test_29, basic_test_set) {
+    BitString first;
+    unsigned char* str1 = new unsigned char[6];
+    str1[0] = '1';
+    str1[1] = '0';
+    str1[2] = '0';
+    str1[3] = '0';
+    str1[4] = '1';
+    str1[5] = '0';
+    bool answer = false;
+    try{
+        first.SetStringAndLength(5, str1);
+    }
+    catch(std::exception &ex){
+        answer = true;
+    }
+    delete[] str1;
+    str1 = nullptr;
+    ASSERT_TRUE(answer);
+}
+
 
 
 
