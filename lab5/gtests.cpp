319d9dacb1639075bd0ba52633fa4622c4bc0ef2
KochevDmitry <65921394+KochevDmitry@users.noreply.github.com>
2023-12-20 22:19:59 +0300

    Update gtests.cpp

--- lab5/gtests.cpp
+++ lab5/gtests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "allocator.h"
 #include "LinkedList.h"
+#include <map>
 
 TEST(test_01, basic_test_set) {
     LinkedList<int> my_list;
@@ -56,7 +57,70 @@ TEST(test_04, basic_test_set) {
     ASSERT_TRUE(c);
 }
 
+TEST(test_05, basic_test_set) {
+    LinkedList<int, AllocatorList<int> > my_list;
+    my_list.push_back(1);
+    my_list.push_back(2);
+    LinkedList<int, AllocatorList<int> >::Iterator it1 = my_list.begin();
+    LinkedList<int, AllocatorList<int> >::Iterator it2 = ++it1;
+    bool c = false;
+    if( *it2 == 2)
+        c = true;
+    ASSERT_TRUE(c);
+}
 
+TEST(test_06, basic_test_set) {
+    LinkedList<int, AllocatorList<int> > my_list;
+    auto it1 = my_list.begin();
+    bool c = false;
+    if(it1 == my_list.end())
+        c = true;
+    ASSERT_TRUE(c);
+}
+
+
+TEST(test_07, basic_test_set) {
+    std::map<int, int, std::less<int>, AllocatorList<std::pair<const int,int>>> my_map;
+    for (int i = 0; i < 10; i ++)
+        my_map[i] = i;
+    bool c = true;
+    for (int i = 0; i < 10; i ++)
+        if (my_map[i] != i)
+            c = false;
+
+    ASSERT_TRUE(c);
+}
+
+TEST(test_08, basic_test_set) {
+    AllocatorList<int> alloc;
+    int c = 1;
+    int * b = &c;
+     ASSERT_THROW(alloc.deallocate(b, 1), std::invalid_argument);
+}
+
+TEST(test_09, basic_test_set) {
+    AllocatorList<int> alloc;
+    int* ptr = alloc.allocate(1);
+    ASSERT_NO_THROW(alloc.deallocate(ptr, 1));
+}
+
+TEST(test_10, basic_test_set) {
+    AllocatorList<int> alloc;
+    int* ptr = alloc.allocate(1);
+    alloc.deallocate(ptr, 1);
+    ASSERT_THROW(alloc.deallocate(nullptr, 1), std::invalid_argument);
+}
+
+TEST(test_11, basic_test_set) {
+    LinkedList<int, AllocatorList<int>> my_list;
+    for (int i = 0; i < 10; i++)
+        my_list.push_back(i);
+    my_list.clear();
+    bool c = false;
+    if(my_list.begin() == my_list.end())
+        c = true;
+    ASSERT_TRUE(c);
+}
 
 int main (int args, char **argv) {
     testing::InitGoogleTest(&args, argv);
